509e65d94a5a3fba674e63d5907e4e186461f412
Eiffel Chong <eiffelchongsy@gmail.com>
2025-03-15 17:37:00 +0800

    adjusted training options

--- basicsr/models/archs/EFNet_att_track_freq_fusion_arch.py
+++ basicsr/models/archs/EFNet_att_track_freq_fusion_arch.py
@@ -166,7 +166,7 @@ class InlineTrackerBlock(nn.Module):
         self.lstm_c = None
 
 
-class EFNet_att_track_freq_fusion_new(nn.Module):
+class EFNet_att_track_freq_fusion(nn.Module):
     def __init__(
         self,
         in_chn=3,
@@ -178,7 +178,7 @@ class EFNet_att_track_freq_fusion_new(nn.Module):
         num_heads=[1, 2, 4],
         use_tracking=True,
     ):
-        super(EFNet_att_track_freq_fusion_new, self).__init__()
+        super(EFNet_att_track_freq_fusion, self).__init__()
         self.depth = depth
         self.fuse_before_downsample = fuse_before_downsample
         self.num_heads = num_heads
@@ -647,7 +647,7 @@ if __name__ == "__main__":
     B, C, H, W = 1, 3, 256, 256
     image = torch.randn(B, C, H, W)
     event = torch.randn(B, 6, H, W)
-    model = EFNet_att_track_freq_fusion_new()
+    model = EFNet_att_track_freq_fusion()
     outs = model(image, event)
     for i, o in enumerate(outs, start=1):
         print(f"Output {i} shape: {o.shape}")
